ce86ac2b6a935a469bcfa406ce7d740d075fe4bb
SCOUTmaster43 <aaron.ellazar@outlook.com>
2025-09-07 20:48:34 -0700

    Update config.js
    added comma

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Steady Intake frontend configuration
 window.STEADY_CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzHGyElT1QreW_i-iaJF1BF_o_wZVvDLdYVN4FFOuxuopoOjsDHgyAAX89WzKhLQXP5LQ/exec"
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzHGyElT1QreW_i-iaJF1BF_o_wZVvDLdYVN4FFOuxuopoOjsDHgyAAX89WzKhLQXP5LQ/exec",
   DEBUG: false,
   SANDBOX: false
 };
