daba6f5dc599427207fc4ec26b213283ff6fdeea
SCOUTmaster43 <aaron.ellazar@outlook.com>
2025-09-07 17:57:42 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,4 @@
-// config.js  (plain script, not a module)
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzHGyElT1QreW_i-iaJF1BF_o_wZVvDLdYVN4FFOuxuopoOjsDHgyAAX89WzKhLQXP5LQ/exec";
+// Replace with your deployed Apps Script URL:
+window.CONFIG = {
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzHGyElT1QreW_i-iaJF1BF_o_wZVvDLdYVN4FFOuxuopoOjsDHgyAAX89WzKhLQXP5LQ/exec"
+};
