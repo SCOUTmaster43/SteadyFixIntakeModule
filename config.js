d9e6662bc9d88c95c80452c5d58f43e1afb2c9d8
SCOUTmaster43 <aaron.ellazar@outlook.com>
2025-09-07 20:28:57 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 // Replace with your deployed Apps Script URL:
-window.CONFIG = {
+window.STEADY_CONFIG = {
   APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzHGyElT1QreW_i-iaJF1BF_o_wZVvDLdYVN4FFOuxuopoOjsDHgyAAX89WzKhLQXP5LQ/exec"
+  DEBUG: false,
+  SANDBOX: false
 };
