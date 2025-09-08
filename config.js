a377b3a04e07c971b1f2c6134766bd38829e4e82
SCOUTmaster43 <aaron.ellazar@outlook.com>
2025-09-07 17:37:57 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,2 @@
-// Wired config (generated)
-window.STEADY_CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzHGyElT1QreW_i-iaJF1BF_o_wZVvDLdYVN4FFOuxuopoOjsDHgyAAX89WzKhLQXP5LQ/exec"
-};
+// config.js  (plain script, not a module)
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzHGyElT1QreW_i-iaJF1BF_o_wZVvDLdYVN4FFOuxuopoOjsDHgyAAX89WzKhLQXP5LQ/exec";
