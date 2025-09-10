472ea95f47d5d531534a6e80ff06383fb6136dec
SCOUTmaster43 <aaron.ellazar@outlook.com>
2025-09-10 10:06:57 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Steady Intake frontend configuration
 window.STEADY_CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbw7VmwIUR9lz0of1Ttid_rkjFOP-v_x6FGg56HVIx0PoHBdRdNbhv63SZWc1sLD1np_ow/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbw4RFPqYM_AxTgw61INPlIJQOVqJZ-IsEVtisG6s8rdSdMmI8MBhJKUWhxkuUvBXj0UbA/exec",
   DEBUG: false,
   SANDBOX: false
 };
